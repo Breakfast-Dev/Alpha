1d913b8f57442ee81cb7a2ec5da79a9cf7657a71
Breakfast-Dev <thdwns0118@naver.com>
2021-12-11 16:53:11 +0900

    Update install.lua

--- apm/test/install.lua
+++ apm/test/install.lua
@@ -1,10 +1,10 @@
-if not _G.Folders and not _G.Folders.UserBin then ResponseInTerminal("E: this package doesn't support this OS"); return end
-if not writefileCooldown and not isfolder then ResponseInTerminal("E: this package doesn't support this OS"); return emd
-if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package")
+if not _G.Folders or not _G.Folders.UserBin then ResponseInTerminal("E: this package doesn't support this OS"); return end
+if not writefileCooldown or not isfolder or not delfile then ResponseInTerminal("E: this package doesn't support this OS"); return end
+if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package") end
 
 	local name = "test.lua"
 	local source = [==[
 		print("HelloWorld")
 	]==]
-	
+
 writefileCooldown(_G.Folders.UserBin .. name, source)
