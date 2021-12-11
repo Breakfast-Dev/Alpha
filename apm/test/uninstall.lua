49c727424e140ef1cf69a6120d0e8ada4d4b779e
Breakfast-Dev <thdwns0118@naver.com>
2021-12-11 16:54:29 +0900

    Fixing interface

--- apm/test/uninstall.lua
+++ apm/test/uninstall.lua
@@ -1,6 +1,6 @@
 if not _G.Folders or not _G.Folders.UserBin then ResponseInTerminal("E: this package doesn't support this OS"); return end
 if not writefileCooldown or not isfolder or not delfile then ResponseInTerminal("E: this package doesn't support this OS"); return end
-if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package") end
+if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package") return end
 
 local name = "test.lua"
 delfile(_G.Folders.UserBin .. name)
