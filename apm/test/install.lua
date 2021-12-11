3cc199aae7bda600167e14dae6cd2f94d4ddd77d
Breakfast-Dev <thdwns0118@naver.com>
2021-12-11 16:51:27 +0900

    Updating interdaces

--- apm/test/install.lua
+++ apm/test/install.lua
@@ -1,4 +1,10 @@
-print(_G.Folders.UserBin .. "test.lua")
-writefileCooldown(_G.Folders.UserBin .. "test.lua", [[
-	print("HelloWorld")
-]])
+if not _G.Folders and not _G.Folders.UserBin then ResponseInTerminal("E: this package doesn't support this OS"); return end
+if not writefileCooldown and not isfolder then ResponseInTerminal("E: this package doesn't support this OS"); return emd
+if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package")
+
+	local name = "test.lua"
+	local source = [==[
+		print("HelloWorld")
+	]==]
+	
+writefileCooldown(_G.Folders.UserBin .. name, source)
--- apm/test/uninstall.lua
+++ apm/test/uninstall.lua
@@ -1,1 +1,6 @@
-delfile(_G.Folders.UserBin .. "test.lua")
+if not _G.Folders or not _G.Folders.UserBin then ResponseInTerminal("E: this package doesn't support this OS"); return end
+if not writefileCooldown or not isfolder or not delfile then ResponseInTerminal("E: this package doesn't support this OS"); return emd
+if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package")
+
+local name = "test.lua"
+delfile(_G.Folders.UserBin .. name)
