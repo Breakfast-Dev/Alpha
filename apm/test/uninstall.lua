82e41a8d1689b1e25b447cfd65eb70a48e20a042
Breakfast-Dev <thdwns0118@naver.com>
2021-12-11 16:53:43 +0900

    Update uninstall.lua

--- apm/test/uninstall.lua
+++ apm/test/uninstall.lua
@@ -1,6 +1,6 @@
 if not _G.Folders or not _G.Folders.UserBin then ResponseInTerminal("E: this package doesn't support this OS"); return end
-if not writefileCooldown or not isfolder or not delfile then ResponseInTerminal("E: this package doesn't support this OS"); return emd
-if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package")
+if not writefileCooldown or not isfolder or not delfile then ResponseInTerminal("E: this package doesn't support this OS"); return end
+if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package") end
 
 local name = "test.lua"
 delfile(_G.Folders.UserBin .. name)
