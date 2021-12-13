a1e4c82690e6052b1789209242663d5880d10b49
Breakfast-Dev <thdwns0118@naver.com>
2021-12-13 17:33:35 +0900

    Updating interface

--- apm/roblox-utils/install.lua
+++ apm/roblox-utils/install.lua
@@ -5,13 +5,15 @@ if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no direc
 local package_name = "roblox-utils"
 local base_repo = "https://raw.githubusercontent.com/Breakfast-Dev/Alpha/main/apm/" .. package_name .."/"
 local get_index = 2
+local sources = {}
 local files = {
 	"rejoin.lua", 
 	"reset.lua", 
 }
 for _, file in pairs(files) do
+	table.insert(sources, game:HttpGet(base_repo .. "files/" .. file))
 	ResponseInTerminal("Get:" .. get_index .." https://raw.githubusercontent.com/Breakfast-Dev/Alpha/main/apm/" .. package_name .. '/files/' .. file)
 end
-for _, file in pairs(files) do
-	writefileCooldown(_G.Folders.UserBin .. file, game:HttpGet(base_repo .. "files/" .. file))
+for i, file in pairs(files) do
+	writefileCooldown(_G.Folders.UserBin .. file, sources[i])
 end
--- apm/roblox-utils/uninstall.lua
+++ apm/roblox-utils/uninstall.lua
@@ -2,5 +2,20 @@ if not _G.Folders or not _G.Folders.UserBin then ResponseInTerminal("E: this pac
 if not writefileCooldown or not isfolder or not delfile then ResponseInTerminal("E: this package doesn't support this OS"); error() end
 if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package"); error() end
 
-local name = "test.lua"
-delfile(_G.Folders.UserBin .. name)
+local package_name = "roblox-utils"
+local base_repo = "https://raw.githubusercontent.com/Breakfast-Dev/Alpha/main/apm/" .. package_name .."/"
+local get_index = 2
+local sources = {}
+local files = {
+	"rejoin.lua", 
+	"reset.lua", 
+}
+for _, file in pairs(files) do
+	table.insert(sources, game:HttpGet(base_repo .. "files/" .. file))
+	ResponseInTerminal("Get:" .. get_index .." https://raw.githubusercontent.com/Breakfast-Dev/Alpha/main/apm/" .. package_name .. '/files/' .. file)
+end
+for i, file in pairs(files) do
+	if isfile(_G.Folders.UserBin .. file) and readfile(_G.Folders.UserBin .. file) == sources[i] then
+		delfile(_G.Folders.UserBin .. file)
+	end
+end
