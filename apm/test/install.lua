47283efc7a82c8643e06d507b25b2fb22c8edc78
Breakfast-Dev <thdwns0118@naver.com>
2021-12-20 15:32:19 +0900

    Update install.lua

--- apm/test/install.lua
+++ apm/test/install.lua
@@ -1,6 +1,6 @@
-if not _G.Folders or not _G.Folders.UserBin then ResponseInTerminal("E: this package doesn't support this OS"); error() end
-if not writefileCooldown or not isfolder or not delfile then ResponseInTerminal("E: this package doesn't support this OS"); error() end
-if not isfolder(_G.Folders.UserBin) then ResponseInTerminal("E: there's no directory for package"); error() end
+if not _G.Folders or not _G.Folders.UserBin then terminal:ResponseInTerminal("E: this package doesn't support this OS"); error() end
+if not writefileCooldown or not isfolder or not delfile then terminal:ResponseInTerminal("E: this package doesn't support this OS"); error() end
+if not isfolder(_G.Folders.UserBin) then terminal:ResponseInTerminal("E: there's no directory for package"); error() end
 
 local name = "test.lua"
 local source = [==[
